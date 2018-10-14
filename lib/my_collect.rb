0c4dfe44e858c3711ca03a5cf3b21dc5afabf013
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-14 15:28:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,22 +1,23 @@
 
-#def my_collect(array)
-
- #i=0 
- #while (i<array.length)
-  #yield array[i] 
-  #if array[i] 
+def my_collect(array)
+ array2=[]
+ i=0 
+ while (i<array.length)
+  array2 << 
+   yield array[i] 
+   if array[i] 
    
-   #i=i+1
- #end
- #array 
-#end
-
-def my_collect(collection)
-  i = 0
-  new_collection = []
-  while i < collection.length
-    new_collection << yield(collection[i])
-    i += 1
-  end
-  new_collection
+   i=i+1
+ end
+ array 
 end
+
+#def my_collect(collection)
+ # i = 0
+  #new_collection = []
+  #while i < collection.length
+    #new_collection << yield(collection[i])
+   # i += 1
+  #end
+ # new_collection
+#end
